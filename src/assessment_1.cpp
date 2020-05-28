ab9a25c49c429318002e3eb4f305aaebe6fa97c1
Wei Guo <wei.parker.guo.sg@gmail.com>
2020-05-28 21:58:14 +0800

    Implemented Ward BRDF for Anisotropic Material rendering~.

--- src/assessment_1.cpp
+++ src/assessment_1.cpp
@@ -27,7 +27,7 @@ static DWORD lastTime;
 static struct timeval lastTime;
 #endif
 
-#define PI 3.14159265 // Should be used from mathlib
+#define PI 3.14159265f // Should be used from mathlib
 
 using namespace std;
 
@@ -55,7 +55,8 @@ inline float sqr(float x) { return x*x; }
 
 /* A faster function for square root. ONLY WORKS for 32 bit float, should be ok for most platforms.
 This is directly taken from the url below, and used only for low-precision scenarios in this scope:
-https://www.codeproject.com/Articles/69941/Best-Square-Root-Method-Algorithm-Function-Precisi */
+https://www.codeproject.com/Articles/69941/Best-Square-Root-Method-Algorithm-Function-Precisi 
+Not used currently for better looks of a smoother sphere */
 float fast_sqrt(float x)
  {
    unsigned int i = *(unsigned int*) &x; 
@@ -79,6 +80,14 @@ float fast_pow(float x, int y){
     else return x*temp*temp;
 }
 
+/* A simple function to make sure every channel for a color stays in [0-1]
+could be disabled for better efficiency */
+void vec3_cull(vec3 r){
+    r[0] = min(1.0f, r[0]);
+    r[1] = min(1.0f, r[1]);
+    r[2] = min(1.0f, r[2]);
+}
+
 /* A simple function that uses a vector to fraction another.
 Basically times each row with each other. */
 void vec3_fraction(vec3 r, const vec3 a, const vec4 b){
@@ -87,6 +96,20 @@ void vec3_fraction(vec3 r, const vec3 a, const vec4 b){
     r[2] = a[2] * b[2];
 }
 
+/* generate lambertian shade with another light source
+ * equation: c = cr(ca + clmax(0, n.l))
+ */
+void gen_lambert_shade(const vec3 ca, const vec3 cr, const vec3 cl, const vec3 n, vec3 l, vec3 c){
+    //normalize light vector
+    vec3_norm(l, l);
+
+    //reflectance
+    float r = max(0.0f, vec3_mul_inner(n,l));
+    vec3_scale(c, cl, r);
+    vec3_add(c, c, ca);
+    //apply reflect fraction
+    vec3_fraction(c, cr, c);
+}
 
 /* generate phong shade based on a precalculated lambertian shade
 equation: c = clambert + clcp(h.n)^p */
@@ -101,22 +124,40 @@ void gen_phong_shade(const vec3 cl, const vec3 cp, const vec3 l, const vec3 e, c
     vec3_scale(cphong, cp, fast_pow(vec3_mul_inner(h,n),p));
     vec3_fraction(cphong, cl, cphong);
     vec3_add(clambert, clambert, cphong);
+    vec3_cull(clambert);
 }
 
+/* generate anisotropic phong shade by dividing the normal into uv directions
+and calculating specular based on the Ward Anisotropic Distribution Model, finally adding the lambertian base shade.
+Notice this can't use our fast_pow since the exponential is float.
+The formula is referenced from this url:
+https://en.wikibooks.org/wiki/GLSL_Programming/Unity/Brushed_Metal */
+void gen_WARD_anisotropic_phong_shade(const vec3 cl, const vec3 cp, const vec3 l, const vec3 e, const vec3 n, const float pu, const float pv, const float y, vec3 clambert){
+    //calculate h
+    vec3 h;
+    vec3_add(h, e, l);
+    vec3_norm(h,h);
 
-/* generate lambertian shade with another light source
- * equation: c = cr(ca + clmax(0, n.l))
- */
-void gen_lambert_shade(const vec3 ca, const vec3 cr, const vec3 cl, const vec3 n, vec3 l, vec3 c){
-    //normalize light vector
-    vec3_norm(l, l);
+    //calculate u and v unit vector
+    vec3 v;
+    vec3 y_v = {0.0f, y, 0.0f};
+    vec3_scale(v, n, vec3_mul_inner(n,y_v));
+    vec3_sub(v, y_v, v);
+    vec3_norm(v,v);
+    vec3 u;
+    vec3_mul_cross(u, v, n);
+    vec3_norm(u,u);
+    
+    //calculate kspecular
+    vec3 kspec;
+    vec3_scale(kspec, cp, sqrt( max(0.0f, vec3_mul_inner(n,l) / vec3_mul_inner(n,l)*vec3_mul_inner(n,e)) )); //first term of the multiplication
+    vec3_scale(kspec, kspec, 1.0f / (4.0f*PI*pu*pv) ); //second term
+    vec3_scale(kspec, kspec, exp( -2.0f * (sqr(vec3_mul_inner(h,u)/pu) + sqr(vec3_mul_inner(h,v)/pv)) / (1.0f+vec3_mul_inner(h,n)) ) ); //thrid term
+    vec3_fraction(kspec, kspec, cl); //control one more time by light color
 
-    //reflectance
-    float r = max((float)0, vec3_mul_inner(n,l));
-    vec3_scale(c, cl, r);
-    vec3_add(c, c, ca);
-    //apply reflect fraction
-    vec3_fraction(c, cr, c);
+    //add specular to lambertian shade
+    vec3_add(clambert, clambert, kspec);
+    vec3_cull(clambert);
 }
 
 //****************************************************
@@ -218,36 +259,40 @@ void drawCircle(float centerX, float centerY, float radius) {
             float x = (i+0.5-centerX);
             float y = (j+0.5-centerY);
 
-            float dist = fast_sqrt(sqr(x) + sqr(y));
+            float dist = sqrt(sqr(x) + sqr(y));
 
             if (dist <= radius) {
 
                 // This is the front-facing Z coordinate
-                float z = fast_sqrt(radius*radius-dist*dist);
+                float z = sqrt(radius*radius-dist*dist);
 
                 //get normalized normal for this point
                 vec3 norm = {x, y, z};
                 vec3_norm(norm, norm);
 
                 //generate lambertian shade
-                vec3 ca = {0.05,0.05,0.05};
-                vec3 cl = {1, 1, 1};
-                vec3 l = {1, 1, 0.8};
-                vec3 cr = {0.9, 0.5, 0.5};
-                vec3 c;
+                vec3 ca = {0.05f,0.05f,0.05f};
+                vec3 cl = {1.0f, 1.0f, 1.0f};
+                vec3 l = {1.0f, 1.0f, 0.8f};
+                vec3 cr = {0.9f, 0.5f, 0.5f};
+                vec3 c = {0.0f, 0.0f, 0.0f};
 
                 //make light static (irrespondent to translation) for more interesting effects
                 //(This won't make sense for directional light conceptually, thus disabled for it by default)
                 l[0]+=Translation[0];
                 l[1]+=Translation[1];
 
-                //generate lambert shade (note this step will also make the light normalized)
+                //generate base lambert shade (note this step will also make the light normalized)
                 gen_lambert_shade(ca, cr, cl, norm, l, c);
 
                 //generate phong from the previous lambertian shade, with assumed view right down z axis
-                vec3 cp = {0.5,0.5,0.5};
-                vec3 e = {0, 0, 1};
-                gen_phong_shade(cl, cp, l, e, norm, 64, c);
+                vec3 cp = {0.9f,0.5f,0.5f};
+                vec3 e = {0.0f, 0.0f, 1.0f};
+                //gen_phong_shade(cl, cp, l, e, norm, 64, c);
+
+                //generate anisotropic shade with pu and pv given (Note: pu and pv has to be culled to [0-1] for this to work)
+                //this can be used to simulate brushed metal or wooden surface, once an appropriate diffuse is given
+                gen_WARD_anisotropic_phong_shade(cl, cp, l, e, norm, 0.2f, 0.3f, y, c);
 
                 setPixel(i, j, c[0], c[1], c[2]);
 
